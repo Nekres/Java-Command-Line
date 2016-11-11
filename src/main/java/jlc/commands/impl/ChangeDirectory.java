d6f0265f5766334089acf591115e7a90669185b3
nekres <shikoryakmisha@mail.ru>
2016-11-11 04:37:53 +0200

    double separator bug fixed when dir equals to '/'

--- src/main/java/jlc/commands/impl/ChangeDirectory.java
+++ src/main/java/jlc/commands/impl/ChangeDirectory.java
@@ -14,40 +14,51 @@ import jlc.exceptions.BadCommandArgumentException;
  *
  * @author desolation
  */
-public class ChangeDirectory extends AbstractCommand implements Command{
+public class ChangeDirectory extends AbstractCommand implements Command {
+
     public static String NAME = "cd";
     private static final int ARG_AMOUNT = 1;
-    private static final String SPLITTER = "/";
+    private static final String SPLITTER = System.getProperty("file.separator");
     private static final String RETURN = "..";
-    private  String arg;
+    private String arg;
 
     public ChangeDirectory(String arg) {
         this.arg = arg;
     }
-    public ChangeDirectory(){
+
+    public ChangeDirectory() {
     }
-    
+
     @Override
-    public void invoke() throws BadCommandArgumentException{
+    public void invoke() throws BadCommandArgumentException {
         String currentDir = System.getProperty("user.dir");
-        if (arg.equals(RETURN) && currentDir.split(SPLITTER).length > 1) {     
-            String result = "";
-            for (int i = 1; i < currentDir.split(SPLITTER).length - 1; i++) {
-                result += SPLITTER + currentDir.split(SPLITTER)[i];
+        if (arg.equals(RETURN) && currentDir.split(SPLITTER).length > 1) {
+            String result = "", dirs[] = currentDir.split(SPLITTER);
+            for (int i = 1; i < dirs.length - 1; i++) {
+                if (!currentDir.equals(SPLITTER)) {
+                    result += SPLITTER + dirs[i];
+                } else {
+                    result += dirs[i];
+                }
             }
-            
-            if(result.equals("") && System.getProperty("os.name").equals("Linux"))
-                result = "/";
+
+            if (result.equals("") && System.getProperty("os.name").equals("Linux")) {
+                result = SPLITTER;
+            }
             System.setProperty("user.dir", result);
             return;
         }
         File nextDir = new File(currentDir);
-        System.out.println(currentDir);
-        if(nextDir.listFiles().length != 0)
-        for (File file : nextDir.listFiles()) {
-            if (arg.toLowerCase().equals(file.getName().toLowerCase()) && new File(currentDir + SPLITTER + arg).isDirectory()) {
-                System.setProperty("user.dir", currentDir + SPLITTER + arg);
-                return;
+        if (nextDir.listFiles().length != 0) {
+            for (File file : nextDir.listFiles()) {
+                if (arg.toLowerCase().equals(file.getName().toLowerCase()) && file.isDirectory()) {
+                    if (!currentDir.equals(SPLITTER)) {
+                        System.setProperty("user.dir", currentDir + SPLITTER + arg);
+                    } else {
+                        System.setProperty("user.dir", currentDir + arg);
+                    }
+                    return;
+                }
             }
         }
         throw new BadCommandArgumentException("Error: No such directory\"" + arg + "\".");
@@ -68,7 +79,8 @@ public class ChangeDirectory extends AbstractCommand implements Command{
     }
 
     @Override
-    public void setOutputPath(PrintStream path) {}
+    public void setOutputPath(PrintStream path) {
+    }
 
     @Override
     public String toString() {
@@ -80,5 +92,4 @@ public class ChangeDirectory extends AbstractCommand implements Command{
         return NAME;
     }
 
-    
 }
